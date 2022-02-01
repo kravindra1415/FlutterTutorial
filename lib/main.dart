67fa64a08c714eef772b08c98946d3a5fc984e2d
kravindra1415 <58881759+kravindra1415@users.noreply.github.com>
2022-02-01 16:15:38 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to flutter..'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
